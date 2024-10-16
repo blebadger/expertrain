e00b6c1c57494daff64a84b3570a2c91173e7232
Badger <badgerbl_p2@pmddtc.onmicrosoft.com>
2024-10-16 13:48:44 +0000

    debugged

--- dataset/collate_datasets.py
+++ dataset/collate_datasets.py
@@ -6,7 +6,7 @@ parser.add_argument('--output_path', type=str)
 parser.add_argument('--n_gpus', type=int)
 
 def collect_and_save(args):
-    paths = [args.output_path + f'_{g}' for g in range(n_gpus)]
+    paths = [args.output_path + f'_{g}' for g in range(args.n_gpus)]
     datasets = [load_from_disk(p) for p in paths]
     all_data = concatenate_datasets(datasets)
     all_data.save_to_disk(args.output_path)
